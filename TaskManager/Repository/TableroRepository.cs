ef12fa2aded8e271b37289e43990534cc63d5d6b
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2026-02-20 15:47:54 -0300

    feat: implementado método CreateTablero

---
+++ TaskManager/Repository/TableroRepository.cs
@@ -0,0 +1,58 @@
+using Microsoft.Data.Sqlite;
+
+public class TableroRepository : ITableroRepository
+{
+    private readonly string _ConnectionString;
+
+    public TableroRepository(string ConnectionString)
+    {
+        _ConnectionString = ConnectionString;
+    }
+
+    public Tablero CreateTablero(int idCreador, string nombreTabla, string descripcionTabla)
+    {
+        Tablero nuevoTablero = new Tablero
+        {
+            IdUsuarioPropietario = idCreador,
+            Nombre = nombreTabla,
+            Descripcion = descripcionTabla
+        };
+
+        string queryStringInsert = @"INSERT INTO Tablero (id_usuario_propietario, nombre, descripcion)
+                                     VALUES(@idCreador, @nombreTabla, @descripcionTabla);";
+
+        string queryStringSelect = @"SELECT MAX(id_tablero) AS id_tablero FROM Tablero;";
+
+        using (SqliteConnection connection = new SqliteConnection(_ConnectionString))
+        {
+            SqliteCommand commandInsert = new SqliteCommand(queryStringInsert, connection);
+
+            connection.Open();
+
+            commandInsert.Parameters.Add(new SqliteParameter("@idCreador", idCreador));
+            commandInsert.Parameters.Add(new SqliteParameter("@nombreTabla", nombreTabla));
+            commandInsert.Parameters.Add(new SqliteParameter("@descripcionTabla", string.IsNullOrEmpty(descripcionTabla) ? DBNull.Value : descripcionTabla));
+
+            int filas = commandInsert.ExecuteNonQuery();
+
+            if (filas == 0)
+            {
+                throw new Exception("No se pudo crear el tablero. Verifique los datos proporcionados.");
+            }
+
+            SqliteCommand commandSelect = new SqliteCommand(queryStringSelect, connection);
+
+            using (SqliteDataReader reader = commandSelect.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    nuevoTablero.IdTablero = Convert.ToInt32(reader["id_tablero"]);
+                }
+            }
+
+            connection.Close();
+        }
+
+        return nuevoTablero;
+    }
+}
