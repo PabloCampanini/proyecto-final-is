d9d1f8c05ecc4137244cd8e57bca90c8951bc4d0
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2026-02-22 14:10:11 -0300

    feat: implementado método CreateTarea

--- TaskManager/Interfaces/ITareaRepository.cs
+++ TaskManager/Interfaces/ITareaRepository.cs
@@ -2,10 +2,10 @@ public interface ITareaRepository
 {
     Tareas CreateTarea(int idTablero);
     void CreateTarea(Tareas nuevaTarea);
-    void UpdateTarea(int idTarea, Tareas tarea);
-    Tareas GetTareaByIdTarea(int idTareaBuscada);
-    List<Tareas> GetAllTareasByIdUsuario(int idUsuario);
-    List<Tareas> GetAllTareasByIdTablero(int idTablero);
-    void DeleteTarea(int idTareaB);
-    void AsignarTarea(int idUsuario, int idTarea);
+    //void UpdateTarea(int idTarea, Tareas tarea);
+    //Tareas GetTareaByIdTarea(int idTareaBuscada);
+    //List<Tareas> GetAllTareasByIdUsuario(int idUsuario);
+    //List<Tareas> GetAllTareasByIdTablero(int idTablero);
+    //void DeleteTarea(int idTareaB);
+    //void AsignarTarea(int idUsuario, int idTarea);
 }
--- TaskManager/Repository/TareaRepository.cs
+++ TaskManager/Repository/TareaRepository.cs
@@ -10,4 +10,43 @@ public class TareaRepository : ITareaRepository
         _ConnectionString = ConnectionString;
     }
 
+    public Tareas CreateTarea(int idTablero)
+    {
+        Tareas nuevaTarea = new Tareas
+        {
+            IdTablero = idTablero,
+        };
+
+        return nuevaTarea;
+    }
+
+    public void CreateTarea(Tareas nuevaTarea)
+    {
+        string queryString = @"INSERT INTO Tarea (id_tablero, nombre, id_estado, descripcion, 
+                                                  id_color, id_usuario_asignado)
+                               VALUES (@idTablero, @Nombre, @idEstado, @Descripcion, @idColor, @idUsuario);";
+
+        using (SqliteConnection connection = new SqliteConnection(_ConnectionString))
+        {
+            SqliteCommand command = new SqliteCommand(queryString, connection);
+
+            connection.Open();
+
+            command.Parameters.Add(new SqliteParameter("@idTablero", nuevaTarea.IdTablero));
+            command.Parameters.Add(new SqliteParameter("@Nombre", nuevaTarea.Nombre));
+            command.Parameters.Add(new SqliteParameter("@idEstado", (int)nuevaTarea.Estado));
+            command.Parameters.Add(new SqliteParameter("@Descripcion", string.IsNullOrEmpty(nuevaTarea.Descripcion) ? DBNull.Value : nuevaTarea.Descripcion));
+            command.Parameters.Add(new SqliteParameter("@idColor", (int)nuevaTarea.Color));
+            command.Parameters.Add(new SqliteParameter("@idUsuario", nuevaTarea.IdUsuarioAsignado ?? (object)DBNull.Value));
+
+            int filas = command.ExecuteNonQuery();
+
+            if (filas == 0)
+            {
+                throw new Exception("Error al crear una tarea. Verifique los datos enviados");
+            }
+
+            connection.Close();
+        }
+    }
 }
