d5fa866dc39410284fc79d15bdf3c3e68b09e407
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2026-02-22 12:37:22 -0300

    feat: creada interfaz ITareaRepository con definición de métodos

---
+++ TaskManager/Interfaces/ITareaRepository.cs
@@ -0,0 +1,11 @@
+public interface ITareaRepository
+{
+    Tareas CreateTarea(int idTablero);
+    void CreateTarea(Tareas nuevaTarea);
+    void UpdateTarea(int idTarea, Tareas tarea);
+    Tareas GetTareaByIdTarea(int idTareaBuscada);
+    List<Tareas> GetAllTareasByIdUsuario(int idUsuario);
+    List<Tareas> GetAllTareasByIdTablero(int idTablero);
+    void DeleteTarea(int idTareaB);
+    void AsignarTarea(int idUsuario, int idTarea);
+}
