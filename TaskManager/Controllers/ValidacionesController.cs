4121e284f7acd863b7094c544f367fc060cd9f74
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2026-02-25 14:53:30 -0300

    feat: agregar validación de Tablero en ValidacionesController

--- TaskManager/Controllers/ValidacionesController.cs
+++ TaskManager/Controllers/ValidacionesController.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ValidacionesController : Controller
 {
+    private readonly ITableroRepository tableroRep;
+
+    public ValidacionesController(ITableroRepository tableroRepository)
+    {
+        tableroRep = tableroRepository;
+    }
     protected bool ValidarRol()
     {
         return HttpContext.Session.GetString("rolSesion") == "Administrador" ? true : false;
@@ -11,5 +17,17 @@ public class ValidacionesController : Controller
     {
         var id = HttpContext.Session.GetInt32("idSesion");
         return id.HasValue ? id.Value : null;
+    }
+    protected bool ValidarCreadorTablero(int idTableroB)
+    {
+        var tableroB = tableroRep.GetTableroByIdTablero(idTableroB);
+        var IdSesion = ValidarSesion();
+
+        if (!IdSesion.HasValue)
+        {
+            return false;
+        }
+
+        return  IdSesion.Value == tableroB.IdUsuarioPropietario ? true : false;
     }
 }
