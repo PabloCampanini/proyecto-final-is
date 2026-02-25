cf2a259a8149b2df74b0333c570b7a89db86f0eb
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2026-02-24 23:49:17 -0300

    feat: agrega LoginVM con validaciones de datos

---
+++ TaskManager/ViewModels/LoginVM.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+public class LoginVM
+{
+    [Required(ErrorMessage = "Debe ingresar un Email válido.")]
+    public string Email {get;set;}
+    
+    [Required(ErrorMessage = "Debe ingresar una contraseña.")]
+    public string Password {get;set;}
+    public string ErrorMessage { get; set; }
+}
