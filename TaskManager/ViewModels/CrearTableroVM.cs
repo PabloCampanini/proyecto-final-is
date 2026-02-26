7c184d93130c014967ec90c7f3db4764c57cf156
PabloCampanini <pablocampanini00@gmail.com>
2026-02-26 20:10:52 -0300

    feat: agrega atributos de validación en ViewModels (#30)

--- TaskManager/ViewModels/CrearTableroVM.cs
+++ TaskManager/ViewModels/CrearTableroVM.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CrearTableroVM
 {
     public int IdCreador { get; set; }
+
+    [Required(ErrorMessage = "El campo 'Nombre del tablero' es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre del tablero no puede tener más de 100 caracteres.")]
     public string NombreTablero { get; set; }
     public string DescripcionTablero { get; set; }
 }
